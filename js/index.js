95af68c219b162a91204505f803b8cc03635febd
mdrifatshahriar1125 <rifatshahriar1125@gmail.com>
2025-03-24 04:12:25 +0600

    complete catagorical section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.getElementById('video-catagory').addEventListener("click",function(){
+document.getElementById('video-catagory').addEventListener("click", function () {
 
 })
 
@@ -30,35 +30,36 @@ document.getElementById('video-catagory').addEventListener("click",function(){
 // [[Prototype]]
 // : 
 // Object
-function videocatagoryload(){
+function videocatagoryload() {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(res=>res.json())
-    .then(data=> showcatagory(data.categories));
+        .then(res => res.json())
+        .then(data => showcatagory(data.categories));
 }
 
-function showcatagory(catagories1){
+function showcatagory(catagories1) {
     catagories1.forEach(element => {
         // console.log(element.category);
-        const newdiv=document.createElement("div");
-        newdiv.innerHTML=`<div  class=" bg-slate-300 py-1 px-2 sm:px-5 sm:py-2 sm:text-xl  rounded-sm ">${element.category}</div>
- `      
- document.getElementById('video-catagory').appendChild(newdiv);
+        const newdiv = document.createElement("div");
+        newdiv.innerHTML = `<div onclick="fetchdifferentcatagoricalvideo(${element.category_id})"  class=" bg-slate-300 py-1 px-2 sm:px-5 sm:py-2 sm:text-xl  rounded-sm ">${element.category}</div>
+ `
+        document.getElementById('video-catagory').appendChild(newdiv);
     });
 }
 
-function fetchallvedio(){
+function fetchallvedio() {
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    .then(res=>res.json())
-    .then(data=> showallvideo(data.videos));
+        .then(res => res.json())
+        .then(data => showallvideo(data.videos));
 }
 
-function showallvideo(videos){
-    const videoload=document.getElementById('video-load');
+function showallvideo(videos) {
+    const videoload = document.getElementById('video-load');
+     videoload.innerHTML="";
 
-videos.forEach(video=>{
-    console.log(video)
-    const newvideo=document.createElement('div');
-    newvideo.innerHTML=`
+    videos.forEach(video => {
+        console.log(video)
+        const newvideo = document.createElement('div');
+        newvideo.innerHTML = `
      <div class="flex flex-col gap-2">
                 <div class=" relative">
               
@@ -92,11 +93,77 @@ videos.forEach(video=>{
 
     `
 
-    videoload.appendChild(newvideo);
-})
+        videoload.appendChild(newvideo);
+    })
 
 }
 
+function fetchdifferentcatagoricalvideo(catagory_id) {
+    console.log(catagory_id)
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${catagory_id}`)
+        .then(res => res.json())
+        .then(data => showcatagoricalvideos(data.category));
+}
+
+function showcatagoricalvideos(video1) {
+    const videoload = document.getElementById('video-load');
+    videoload.innerHTML="";
+
+    if(video1.length<1){
+       const newvideo = document.createElement('div');
+        newvideo.innerHTML = `
+        
+           <div class="flex flex-col justify-center items-center py-20">
+                <div class="mx-auto"><img src="assets/Icon.png" alt=""></div>
+                <div class="mx-auto text-2xl font-bold text-center">Oops!! Sorry, There is no <br> content here</div>
+
+            </div>
+        ` 
+        videoload.appendChild(newvideo);
+        return;
+    }
+
+    video1.forEach(video => {
+        // console.log(video)
+        const newvideo = document.createElement('div');
+        newvideo.innerHTML = `
+     <div class="flex flex-col gap-2">
+                <div class=" relative">
+              
+                    <img class="w-full h-56 object-cover rounded-lg" src="${video.thumbnail}" alt="">
+     
+                    <p class="absolute bottom-2 right-2 text-white bg-black px-2 py-1 rounded-md">3hrs 56 min ago</p>
+                    
+                </div>
+                <div class="flex gap-1">
+                    <div>
+                       
+                        <img class="rounded-full w-10 h-10" src="${video.authors[0].profile_picture}" alt="">
+                        
+                    </div>
+                        
+                    <div>
+                        <p class="font-semibold text-xl">${video.title}</p>
+                        <div class="flex items-center">
+                            <p>${video.authors[0].profile_name}</p>
+                            <p>${video.authors[0].verified ? '<img class="h-5"  src="assets/varified icon.png" alt="">' : ''}</p>
+                          
+                            
+                            
+                        </div>
+                        <p class="flex gap-3">${video.others.views} views </p>
+
+                    </div>
+                </div>
+
+            </div>
+
+    `
+
+        videoload.appendChild(newvideo);
+    })
+
+}
 
 fetchallvedio()
 videocatagoryload();
